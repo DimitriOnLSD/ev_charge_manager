c02d0001de97bc03c42ac225910fe21076ed6664
Paulo Sousa <100768973+DimitriOnLSD@users.noreply.github.com>
2023-12-16 06:20:12 +0000

    changed date type

--- src/main/java/com/mycompany/projeto_pc2/ChargingSession.java
+++ src/main/java/com/mycompany/projeto_pc2/ChargingSession.java
@@ -1,17 +1,19 @@
 package com.mycompany.projeto_pc2;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  *
- * @author Paulo Sousa | João Domingos
+ * @authors Paulo Sousa | João Domingos
  */
 public class ChargingSession {
     ChargingStation chargingStation;
     Vehicle vehicle;
     Client client;
     protected int session_code, type_of_payment;
-    protected Date start_time, finish_time, time_transaction;
+    protected LocalDateTime start_time;
+    protected LocalDateTime finish_time;
+    protected LocalDateTime time_transaction;
     protected double energy_consumed, session_cost;
     protected String settlement_status;
 
@@ -19,9 +21,11 @@ public class ChargingSession {
                            Vehicle vehicle,
                            Client client,
                            int session_code,
-                           Date start_time,
-                           Date finish_time,
-                           String settlement_status) {
+                           LocalDateTime start_time,
+                           LocalDateTime finish_time,
+                           String settlement_status,
+                           double energy_consumed,
+                           double session_cost) {
         this.chargingStation = chargingStation;
         this.vehicle = vehicle;
         this.client = client;
@@ -29,31 +33,33 @@ public class ChargingSession {
         this.start_time = start_time;
         this.finish_time = finish_time;
         this.settlement_status = settlement_status;
+        this.energy_consumed = energy_consumed;
+        this.session_cost = session_cost;
     }
 
-    public int getSessionCode()                               { return session_code; }
-    public void setSessionCode(int session_code)              { this.session_code = session_code; }
+    public int getSessionCode()                                        { return session_code; }
+    public void setSessionCode(int session_code)                       { this.session_code = session_code; }
 
-    public Date getStartTime()                                { return start_time; }
-    public void setStartTime(Date start_time)                 { this.start_time = start_time; }
+    public LocalDateTime getStartTime()                                { return start_time; }
+    public void setStartTime(LocalDateTime start_time)                 { this.start_time = start_time; }
 
-    public Date getFinishTime()                               { return finish_time; }
-    public void setFinishTime(Date finish_time)               { this.finish_time = finish_time; }
+    public LocalDateTime getFinishTime()                               { return finish_time; }
+    public void setFinishTime(LocalDateTime finish_time)               { this.finish_time = finish_time; }
 
-    public double getEnergyConsumed()                         { return energy_consumed; }
-    public void setEnergyConsumed(double energy_consumed)     { this.energy_consumed = energy_consumed; }
+    public double getEnergyConsumed()                                  { return energy_consumed; }
+    public void setEnergyConsumed(double energy_consumed)              { this.energy_consumed = energy_consumed; }
 
-    public double getSessionCost()                            { return session_cost; }
-    public void setSessionCost(double session_cost)           { this.session_cost = session_cost; }
+    public double getSessionCost()                                     { return session_cost; }
+    public void setSessionCost(double session_cost)                    { this.session_cost = session_cost; }
 
-    public String getSettlementStatus()                       { return settlement_status; }
-    public void setSettlementStatus(String settlement_status) { this.settlement_status = settlement_status; }
+    public String getSettlementStatus()                                { return settlement_status; }
+    public void setSettlementStatus(String settlement_status)          { this.settlement_status = settlement_status; }
 
-    public int getTypeOfPayment()                             { return type_of_payment; }
-    public void setTypeOfPayment(int type_of_payment)         { this.type_of_payment = type_of_payment; }
+    public int getTypeOfPayment()                                      { return type_of_payment; }
+    public void setTypeOfPayment(int type_of_payment)                  { this.type_of_payment = type_of_payment; }
 
-    public Date getTimeOftransaction()                        { return time_transaction; }
-    public void setTimeOftransaction(Date time_transaction)   { this.time_transaction = time_transaction; }
+    public LocalDateTime getTimeOftransaction()                        { return time_transaction; }
+    public void setTimeOftransaction(LocalDateTime time_transaction)   { this.time_transaction = time_transaction; }
 
     @Override
     public String toString() {
@@ -63,10 +69,10 @@ public class ChargingSession {
         str.append("Estacao a usar: " + chargingStation.getAddress() + " (Cod: " + chargingStation.getStationCode() + ")\n");
         str.append("Hora de inicio: " + start_time + "\n");
         str.append("Hora de termino: " + finish_time + "\n");
-        str.append("Energia consumida: " + energy_consumed + "\n");
+        str.append("Energia consumida: " + energy_consumed + " kW/h\n");
         str.append("Custo da sessao: " + session_cost + " euros\n");
         str.append("Estado de liquidacao: " + settlement_status + "\n");
-        if (settlement_status.equals("Pago")) {
+        if (getSettlementStatus().equals("Pago")) {
             switch (type_of_payment) {
                 case 1:
                     str.append("Tipo de pagamento: MB WAY\n");
